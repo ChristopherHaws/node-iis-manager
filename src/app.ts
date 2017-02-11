a8bcdcf439c9929f2b765482dd7e2d47cfa6820c
ChristopherHaws <cribs2@gmail.com>
2017-02-10 20:07:29 -0800

    Add app methods

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,44 @@
 import { AppCmd } from './app-cmd'
 
-export interface IListAppResult {
+export interface App {
 	path: string;
 	name: string;
 	appPoolName: string;
 	siteName: string;
 }
 
+export interface AppOptions {
+	name: string;
+	virtualPath: string;
+	physicalPath: string;
+}
+
 export class App {
+	public async add(options: AppOptions): Promise<void> {
+		let command = new AppCmd();
+
+		let results = await command
+			.arg('add')
+			.arg('app')
+			.arg('/site.name', options.name)
+			.arg('/path', options.virtualPath)
+			.arg('/physicalPath', options.physicalPath)
+			.exec();
+	}
+
 	public async exists(name: string): Promise<boolean> {
 		let apps = await this.list();
 
 		return apps.some(x => x.name === name);
 	}
 
-	public async list(): Promise<IListAppResult[]> {
+	public async list(): Promise<App[]> {
 		let command = new AppCmd();
 
 		let results = await command
 			.arg('list')
 			.arg('app')
-			.exec(this.mapSiteResults);
+			.exec(this.map);
 
 		return results.value;
 	}
@@ -53,14 +71,49 @@ export class App {
 		}
 	}
 
-	private mapSiteResults(value: any): IListAppResult[] {
+	public async setAppPool(appName: string, appPoolName: string): Promise<void> {
+		let command = new AppCmd();
+
+		let results = await command
+			.arg('set')
+			.arg('app')
+			.arg('/app.name', appName)
+			.arg('/applicationPool', appPoolName)
+			.exec();
+	}
+
+	public async setWindowsAuthentication(appPath: string, enable: boolean): Promise<void> {
+		let command = new AppCmd();
+
+		let results = await command
+			.arg('set')
+			.arg('config')
+			.arg(appPath)
+			.arg('/section', 'windowsAuthentication')
+			.arg('/enabled', enable.toString())
+			.exec();
+	}
+
+	public async setAnonymousAuthentication(appPath: string, enable: boolean): Promise<void> {
+		let command = new AppCmd();
+
+		let results = await command
+			.arg('set')
+			.arg('config')
+			.arg(appPath)
+			.arg('/section', 'anonymousAuthentication')
+			.arg('/enabled', enable.toString())
+			.exec();
+	}
+
+	private map(value: any): App[] {
 		return value.appcmd.APP.map(app => {
 			return {
 				path: app.$['path'],
 				name: app.$['APP.NAME'],
 				appPoolName: app.$['APPPOOL.NAME'],
 				siteName: app.$['SITE.NAME']
-			} as IListAppResult
+			} as App
 		});
 	}
 }
