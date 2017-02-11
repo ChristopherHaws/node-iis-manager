4a1490453d45521f64e5b24cd1af61f61cce6902
ChristopherHaws <cribs2@gmail.com>
2017-02-10 22:30:31 -0800

    Make test cleanup after itself

--- test/site-tests.ts
+++ test/site-tests.ts
@@ -1,25 +1,29 @@
 require('mocha');
 require('chai').should();
+import * as uuid from 'uuid';
 import { sites, SiteOptions } from '../';
 
 describe('site', () => {
 	it('can be retrieved after being created', async () => {
-		let exists = await sites.exists('TestSite');
-		if (exists) {
-			await sites.remove('TestSite');
+		let siteName = uuid();
+
+		try {
+			await sites.add({
+				name: siteName,
+				protocol: 'http',
+				host: siteName,
+				port: 80,
+				path: 'C:\\inetpub\\wwwroot'
+			} as SiteOptions);
+
+			let site = await sites.get(siteName);
+
+			site.name.should.equal(siteName);
+			site.bindings.should.equal(`http/*:80:${siteName}`);
+		} finally {
+			if (await sites.exists(siteName)) {
+				await sites.remove(siteName);
+			}
 		}
-
-		await sites.add({
-			name: 'TestSite',
-			protocol: 'http',
-			host: 'testsiteurl',
-			port: 12345,
-			path: 'C:\\inetpub\\wwwroot'
-		} as SiteOptions);
-
-		let site = await sites.get('TestSite');
-
-		site.name.should.equal('TestSite');
-		site.bindings.should.equal('http/*:12345:testsiteurl');
 	});
 });
